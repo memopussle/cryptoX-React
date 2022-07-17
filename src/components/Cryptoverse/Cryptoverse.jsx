def20608345f1de011198a29407dc527f8ed1fe1
thugiang <xanhtham.cuc@gmail.com>
2022-07-18 01:44:09 +1200

    fetching API from RAPID API and using RTK query from Redux toolkit

--- src/components/Cryptoverse/Cryptoverse.jsx
+++ src/components/Cryptoverse/Cryptoverse.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { useEffect } from "react";
+import { useGetCryptosQuery } from "../../services/cryptoApi";
 import "./Cryptoverse.scss";
 
 const Cryptoverse = () => {
-  return <div>Cryptoverse</div>;
+  const count = 10;
+  const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
+  console.log(cryptoList);
+
+  const [cryptos, setCryptos] = useState();
+  //loop through list
+  useEffect(() => {
+
+  }, [])
+
+  if (isFetching) return "Loading...";
+  return (
+    <section className="crypto">
+      <h1>Cryptoverse</h1>
+      
+    </section>
+    );
 };
 
 export default Cryptoverse;
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,25 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "a6ebbd3eb6msh24556d00e696102p156cd1jsn0869fe532aa0",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders }); //url = url,headers = cryptoApiHeaders
+//headers: Object { "X-RapidAPI-Key": "03f8ed297fmshe38d28b4515a3a0p1cd970jsn5702025fb60a", "X-RapidAPI-Host": "coinranking1.p.rapidapi.com" }
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }), //baseUrl = baseUrl variable above
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins`), //will add endpoint to url https://coinranking1.p.rapidapi.com/coins
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi; //creat a hook to get all data for the query above
+
+console.log(useGetCryptosQuery);
